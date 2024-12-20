0fd55d5702403c5ffe6cdb8d82725e4b270acf80
Prathamesh Chaubey <prathameshchaubey@2wheelr.com>
2024-12-20 18:08:49 +0530

    chore:import fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import HeroSection from '../components/Home/HeroSection'
 import CompanyDetails from '../components/Home/CompanyDetails'
 import Service from '../components/Home/Service'
-import CompanyLogos from '../components/Home/Companylogos'
+import CompanyLogos from '../components/Home/CompanyLogos'
 
 import { homeServiceData1, homeServiceData2 } from '../utils/data';
 
